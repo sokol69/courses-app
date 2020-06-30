8c6ea526a9760d1eb1668d0487b3880b295151f8
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2020-06-30 08:45:48 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const ehbs = require("express-handlebars");
+
+const app = express();
+
+const hbs = ehbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Home page",
+    isHome: true,
+  });
+});
+
+app.get("/courses", (req, res) => {
+  res.render("courses", {
+    title: "Courses",
+    isCourses: true,
+  });
+});
+
+app.get("/add", (req, res) => {
+  res.render("add", {
+    title: "Add course",
+    isAdd: true,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
