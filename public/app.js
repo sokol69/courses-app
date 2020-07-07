54d3ca422011ce805086d9858dca33ecafb5ba7c
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2020-07-07 08:38:22 +0300

    Finished orders page

--- public/app.js
+++ public/app.js
@@ -1,12 +1,27 @@
 const toCurrency = (price) => {
-  return new Intl.NumberFormat("ru-RU", {
-    currency: "rub",
+  return new Intl.NumberFormat("en-EN", {
+    currency: "USD",
     style: "currency",
   }).format(price);
 };
 
+const toDate = (date) => {
+  return new Intl.DateTimeFormat("en-EN", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(new Date(date));
+};
+
 document.querySelectorAll(".price").forEach((node) => {
   node.textContent = toCurrency(node.textContent);
+});
+
+document.querySelectorAll(".date").forEach((node) => {
+  node.textContent = toDate(node.textContent);
 });
 
 const $card = document.querySelector("#card");
